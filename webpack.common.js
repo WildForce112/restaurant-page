fea9b6f9048db625e4b90eea7453001903c3c375
WildForce112 <81035327+WildForce112@users.noreply.github.com>
2025-12-18 22:12:31 +0700

    feat: split config file

--- webpack.common.js
+++ webpack.common.js
@@ -1,24 +1,21 @@
-// webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
+
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
     }),
   ],
+
   module: {
     rules: [
       {
@@ -27,5 +24,4 @@ module.exports = {
       },
     ],
   },
-
 };
